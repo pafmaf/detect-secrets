ec9a9ca5ed250d5680a4ced0e00888280087f899
Kirill Wedenin <kirill.wedenin@codecentric.de>
2025-02-27 19:56:55 +0100

    fix: catch NPM tokens with `npm_` prefix
    access tokens from npmjs now look like `npm_XXXX..`

--- detect_secrets/plugins/npm.py
+++ detect_secrets/plugins/npm.py
@@ -14,4 +14,5 @@ class NpmDetector(RegexBasedDetector):
         # npmrc authToken
         # ref. https://stackoverflow.com/questions/53099434/using-auth-tokens-in-npmrc
         re.compile(r'\/\/.+\/:_authToken=\s*((npm_.+)|([A-Fa-f0-9-]{36})).*'),
+        re.compile(r'npm_[A-Za-z0-9]{36}'),
     ]
